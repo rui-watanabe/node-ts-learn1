06ce89b8854cc58ee937a411ecdc145e777350b1
rui-watanabe <wataru610806@gmail.com>
2020-06-27 13:58:07 +0900

    implement todo get, update, delete

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 exports.createTodo = (req, res, next) => {
@@ -8,4 +8,26 @@ exports.createTodo = (req, res, next) => {
     const newTodo = new todo_1.Todo(Math.random().toString(), text);
     TODOS.push(newTodo);
     res.status(201).json({ message: 'create todo', createTodo: newTodo });
+};
+exports.getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updateText = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('not find target todo');
+    }
+    TODOS[todoIndex] = new todo_1.Todo(todoId, updateText);
+    res.json({ message: 'updated todo', updatedTodo: TODOS[todoIndex] });
+};
+exports.deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('not find target todo');
+    }
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: 'deleted todo' });
 };
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -12,3 +12,38 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.status(201).json({ message: 'create todo', createTodo: newTodo });
 }
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS })
+}
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updateText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if(todoIndex < 0)
+  {
+    throw new Error('not find target todo')
+  }
+
+  TODOS[todoIndex] = new Todo(todoId, updateText);
+
+  res.json({ message: 'updated todo', updatedTodo: TODOS[todoIndex] });
+}
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if(todoIndex < 0)
+  {
+    throw new Error('not find target todo')
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: 'deleted todo' });
+}
