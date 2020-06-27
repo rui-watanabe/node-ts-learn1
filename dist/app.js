d02caaab46e4a6547930b508846bf9e8db843854
rui-watanabe <wataru610806@gmail.com>
2020-06-27 13:12:59 +0900

    add middleware with type

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const todo_1 = __importDefault(require("./routes/todo"));
 const app = express_1.default();
+app.use('/todos', todo_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 app.listen(3000);
